99413e09a8702566938d8b8eb35eddea13471e2b
vegan13 <vegan13@o2.pl>
2017-03-27 22:00:40 +0200

    cytaty na tweetera

---
+++ js/scripts.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+
+    // zmienne
+    var tweetLink = "https://twitter.com/intent/tweet?text=";
+    var quoteUrl = "http://api.forismatic.com/api/1.0/?method=getQuote&key=867576&format=jsonp&lang=en&jsonp=?";
+
+    // funkcje
+    
+    function getQuote() {
+        $.getJSON(quoteUrl, createTweet);
+    }
+    
+    function createTweet(input) {
+        
+        var tweetText = "Quote of the day - " + input.quoteText + " Author: " + input.quoteAuthor;
+        
+        if (!input.quoteAuthor.length) {
+            input.quoteAuthor = "Unknown author";
+        }
+        
+        if (tweetText.length > 140) {
+            getQuote();
+        } else {
+            var tweet = tweetLink + encodeURIComponent(tweetText);
+            $('.quote').text(input.quoteText);
+            $('.author').text("Autor cytatu: " + input.quoteAuthor);
+            $('.tweet').attr('href', tweet);
+        }
+        
+        $('.tweet').attr('href', tweet);
+        
+    }
+    
+	getQuote();
+    
+	$('.trigger').click(function() {
+		getQuote();
+	})
+    
+    
+    
+});
